28ffbaf91603fc0d0db81ae9ff81e7d2da8870c9
Hannah <144931749+conranh@users.noreply.github.com>
2026-04-28 11:19:32 -0400

    Changed cube from having cubes in groups to cubes being individual/cleaned up material so colors are more accurate

--- main.js
+++ main.js
@@ -12,41 +12,66 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = [
-    new THREE.MeshBasicMaterial({color: 0x009A44}), // right GREEN
-    new THREE.MeshBasicMaterial({color: 0x003DA5}), // left BLUE
-    new THREE.MeshBasicMaterial({color: 0xBA0C2F}), // top RED
-    new THREE.MeshBasicMaterial({color: 0xFE5000}), // bottom ORANGE
-    new THREE.MeshBasicMaterial({color: 0xffffff}), // front WHITE
-    new THREE.MeshBasicMaterial({color: 0xFFD700}) // back YELLOW
-];
+
+const colors = {
+    red: 0xBA0C2F,
+    orange: 0xFE5000,
+    yellow: 0xFFD700,
+    green: 0x009A44,
+    blue: 0x003DA5,
+    white: 0xffffff,
+    black: 0x000000
+};
+
+const materials = {
+    right: new THREE.MeshBasicMaterial({ color: colors.blue }),
+    left: new THREE.MeshBasicMaterial({ color: colors.green }),
+    top: new THREE.MeshBasicMaterial({ color: colors.white }),
+    bottom: new THREE.MeshBasicMaterial({ color: colors.yellow }),
+    front: new THREE.MeshBasicMaterial({ color: colors.red }),
+    back: new THREE.MeshBasicMaterial({ color: colors.orange })
+}
+
 const outlineMaterial = new THREE.MeshBasicMaterial({
     color: 0x000000,
     side: THREE.BackSide,
 });
 
-addCube();
-function addCube() {
-    for(let z = 0; z < 3; z++) {
-        for(let x = 0; x < 3; x++) {
-            for(let y = 0; y < 3; y++) {
-                const cube = new THREE.Mesh(geometry, material);
-    
-                cube.position.set(x * 1.05, y * 1.05, z * 1.05);
-                scene.add(cube);
 
-                const outline = new THREE.Mesh(geometry, outlineMaterial);
-                outline.scale.set(1.03 * 1.05, 1.03 * 1.05, 1.03 * 1.05);
-                outline.position.copy(cube.position);
-                scene.add(outline);
-            }
+const cubes = [];
+const spacing = 1.05;
+
+function createCube(x, y, z) {
+    const cube = new THREE.Mesh(geometry, [
+        x === 1 ? materials.right : colors.black,
+        x === -1 ? materials.left : colors.black,
+        y === 1 ? materials.top : colors.black,
+        y === -1 ? materials.bottom : colors.black,
+        z === 1 ? materials.front : colors.black,
+        z === -1 ? materials.back : colors.black
+    ]);
+
+    cube.userData.grid = { x, y, z };
+    cube.position.set(x * spacing, y * spacing, z * spacing);
+
+    const outline = new THREE.Mesh(geometry, outlineMaterial);
+    outline.scale.set(1.03 * spacing, 1.03 * spacing, 1.03 * spacing);
+    cube.add(outline);
+
+    scene.add(cube);
+    cubes.push(cube);
+}
+
+for(let x = -1; x <= 1; x++) {
+    for(let y = -1; y <= 1; y++) {
+        for(let z = -1; z <= 1; z++) {
+            createCube(x, y, z);
         }
     }
 }
 
 camera.position.z = 10;
 camera.position.x = 3;
-
 
 function animate(time) {
     controls.update();
