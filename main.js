36a41837fcdbdf8949578dd6136921943fa222d1
Hannah <144931749+conranh@users.noreply.github.com>
2026-05-02 13:39:02 -0400

    Modified camera starting position

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x545454);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10;
-camera.position.x = 3;
+camera.position.z = 7;
+camera.position.x = 4;
+camera.position.y = 4;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
