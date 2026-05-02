2535d095e1af5b01fe3814ac26304a66f763a335
Hannah <144931749+conranh@users.noreply.github.com>
2026-05-02 15:17:49 -0400

    Converted this into a react + vite project/made the scene a component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import RubiksScene from './components/RubiksScene'
+
+function App() {
+  return (
+    <RubiksScene />
+  )
+}
+
+export default App
